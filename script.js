40a86013d0c480c3506c5db4e24d1a5099c4e8db
Cora-Jane Meade <corascompanycode@gmail.com>
2021-08-05 15:25:42 +0100

    added interchangeable script

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById("fullName").innerHTML = "Cora-Jane Meade";
+var linkedIn = document.getElementById("linkedInLink");
+linkedIn.setAttribute('href', 'https://www.linkedin.com/in/cora-jane-meade-13ba6b159/');
+
+
+document.getElementById("briefDescription").innerHTML =
+    "Hi! I'm Cora. I am 22 years old and from the only island town in Ireland, Enniskillen.\
+I'm a computer science graduate with a passion for technology, particularly developing iOS apps.\
+Below you'll find different projects I have worked on, most of which are simplistic and primarly for learning purposes";
+
+
+document.getElementById("profilePicture").src = "images/pic01.jpg";
+
+
+
+// document.getElementById("fullName").innerHTML = "Example Name";
+// var linkedIn = document.getElementById("linkedInLink");
+// linkedIn.setAttribute('href', 'https://www.linkedin.com/in/cora-jane-meade-13ba6b159/');
+
+// // github link
+
+
+// document.getElementById("briefDescription").innerHTML =
+//     "Brief Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at libero imperdiet, euismod risus nec,\
+//     dignissim tortor. Praesent convallis massa ut neque scelerisque pulvinar. Donec ac purus consequat, cursus libero a, tincidunt arcu.\
+//     Mauris dapibus ligula ex, consequat volutpat quam ultrices in. Praesent erat tellus, condimentum a nisi rhoncus, efficitur elementum diam.\
+//     Phasellus ut sapien nec arcu sagittis viverra a ut ex. Maecenas non luctus ipsum. Sed iaculis sit amet ipsum eu aliquam.";
+
+
+// document.getElementById("profilePicture").src = "images/pic01example.jpg";
